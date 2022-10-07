cb1ece7ef88e24899060d2e20151581ecf20fdb7
Cat <catholm1@outlook.com>
2022-10-07 19:35:23 +0200

    frontend change

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,13 @@
+
 import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
+import ReactDOM  from 'react-dom/client';
 import './index.css';
+import App from './App';
 
-const container = document.getElementById('root');
-const root = createRoot(container);
+import { store } from './app/store';
+import { Provider } from 'react-redux';
 
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
@@ -15,5 +15,4 @@ root.render(
     </Provider>
   </React.StrictMode>
 );
-
 
