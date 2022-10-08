ca7f5d81f5adc43764bd15b46f7aea63f9dc7171
Cat <catholm1@outlook.com>
2022-10-08 22:18:21 +0200

    setting goals

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,6 +1,9 @@
+// https://www.youtube.com/watch?v=UXjMo25Nnvc&list=PLillGF-RfqbbQeVSccR9PGKHzPJSWqcsm&index=4 time: 10:16
+
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom'
 import { useSelector } from 'react-redux'
+import GoalForm from '../components/GoalForm';
 
 
 function Dashboard() {
@@ -18,7 +21,13 @@ function Dashboard() {
 
 
   return (
-    <div>Dashboard</div>
+    <>
+     <section className='heading'>
+        <h1>Welcome {user && user.name}</h1>
+        <p>Goals Dashboard</p>
+     </section>
+     <GoalForm />
+    </>
   )
 }
 
