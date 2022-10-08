553f9230db859165f02b56c227e237eebe017ef7
Cat <catholm1@outlook.com>
2022-10-08 22:43:30 +0200

    Get user goals

--- frontend/src/features/goals/goalSlice.js
+++ frontend/src/features/goals/goalSlice.js
@@ -24,7 +24,31 @@ export const createGoal = createAsyncThunk('goals/create',
           error.toString()
         return thunkAPI.rejectWithValue(message)
       }
-})
+    }
+)
+
+//Get user goals
+export const getGoals = createAsyncThunk(
+    'goals/getAll',
+    async (_, thunkAPI) => {
+      try {
+        const token = thunkAPI.getState().auth.user.token
+        return await goalService.getGoals(token)
+      } catch (error) {
+        const message =
+          (error.response &&
+            error.response.data &&
+            error.response.data.message) ||
+            error.message ||
+            error.toString()
+        return thunkAPI.rejectWithValue(message)
+      }
+    }
+)
+
+
+
+
 
 export const goalSlice = createSlice({
     name: 'goal',
@@ -47,6 +71,21 @@ export const goalSlice = createSlice({
               state.isError = true
               state.message = action.payload
             })
+            
+            .addCase(getGoals.pending, (state) => {
+                state.isLoading = true
+            })
+            .addCase(getGoals.fulfilled, (state, action) => {
+                state.isLoading = false
+                state.isSuccess = true
+                state.goals = action.payload
+            })
+            .addCase(getGoals.rejected, (state, action) => {
+                state.isLoading = false
+                state.isError = true
+                state.message = action.payload
+            })
+
     }
 })
 
